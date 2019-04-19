ad46eab21517d66e0856e7ad83545b264ba911aa
Greg Vaughn <gvaughn@gmail.com>
2019-04-19 13:40:59 -0500

    copy/paste clipboard in iex

--- iex.exs
+++ iex.exs
@@ -1,2 +1,29 @@
 # show erlang single quoted char lists as lists
-#IEx.configure inspect: [char_lists: false]
+# IEx.configure inspect: [char_lists: false]
+defmodule Clipboard do
+  # Shamelessly stolen from https://github.com/jayjun/clipboard
+
+  def paste() do
+    {res, _} = System.cmd("/usr/bin/pbpaste", [])
+    res
+  end
+
+  def copy(val) do
+    port = Port.open({:spawn_executable, "/usr/bin/pbcopy"}, [:binary, args: []])
+
+    value =
+      case val do
+        val when is_binary(val) ->
+          val
+
+        val ->
+          val
+          |> Inspect.Algebra.to_doc(%Inspect.Opts{limit: :infinity})
+          |> Inspect.Algebra.format(:infinity)
+      end
+
+    send(port, {self(), {:command, value}})
+    send(port, {self(), :close})
+    val
+  end
+end
